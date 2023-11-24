afdbf47eec06a1c69777f02f3f8540d7c04e3a46
Thomas Rands <132161715+TomRands@users.noreply.github.com>
2023-11-24 13:03:54 +0000

    Updated with clean code

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -16,17 +16,4 @@ public class App {
     public String getMessage() {
         return MESSAGE;
     }
-}public class App {
-
-    private static final String MESSAGE = "Hello World!";
-
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(MESSAGE);
-    }
-
-    public String getMessage() {
-        return MESSAGE;
-    }
 }
